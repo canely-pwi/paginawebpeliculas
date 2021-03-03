2b5b29500cf34bae45ce82ee209a39247a127dbf
canely-pwi <80010585+canely-pwi@users.noreply.github.com>
2021-03-03 23:58:39 +0100

    Delete PhpProject1 directory

--- PhpProject1/javascript.js
+++
@@ -1,8 +0,0 @@
-document . getElementById ("mensaje"). innerHTML = "Disfruten de todas las peliculas y no se pierdan de ninguna";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
